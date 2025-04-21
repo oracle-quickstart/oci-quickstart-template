f14cb63748cbdafb80cf301cfc26769c8560ebf6
Collin Poczatek <collin.poczatek@gmail.com>
2025-04-21 12:15:40 -0700

    update tf/provider version

--- versions.tf
+++ versions.tf
@@ -1,9 +1,9 @@
 terraform {
- required_version = ">= 1.0.0"
+ required_version = ">= 1.5.0"
  required_providers {
-     # Recommendation from ORM / OCI provider teams
           oci = {
-             version =">= 4.21.0"
+             source = "oracle/oci"
+             version =">= 6.30"
           }
  }
 }
